c97cfd0d8dce81b6a7b949fb55caf38f4e46c440
krishnatanpure <rushikeshtanpure10012002@gmail.com>
2023-10-03 22:26:36 +0530

    remove

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = app.listen(port, () => {
 });
 const io = socket(server, {
     cors: {
-        origin: "https://chatapprushi.netlify.app/",
+        origin: "https://chatapprushi.netlify.app",
         // origin: "http://localhost:3000",
 
         Credential: true
