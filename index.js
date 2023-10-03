3fa030f1dc0ac2f2fad8b2a3f70568adb966db73
krishnatanpure <rushikeshtanpure10012002@gmail.com>
2023-10-03 15:32:35 +0530

    add frontend link

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = app.listen(port, () => {
 });
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chatapprushi.netlify.app/chat",
         Credential: true
     }
 })
